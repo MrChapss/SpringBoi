49e826cdfc0a57f276abccdfb810709054e4f9d7
Chapss <palmakarsten@gmail.com>
2026-01-09 22:44:56 +0800

    Learnings: 1. Dependecy injection (injecting service class into controller) 2. Using interface in service class and make the controller depend on interface ONLY

--- src/main/java/typeshi/SpringBooty/controller/TestController.java
+++ src/main/java/typeshi/SpringBooty/controller/TestController.java
@@ -2,22 +2,34 @@ package typeshi.SpringBooty.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import typeshi.SpringBooty.service.TestInterface;
 
 @RestController
 public class TestController {
+	private final TestInterface service;
+	
+	@Autowired
+	public TestController (TestInterface service) {
+		this.service=service;
+	}
 	
 	@GetMapping("/hello")
 	public String hello(String name) {
-		return "Hello, " + name;
-	}
-	
-	@GetMapping("/hello/hi")
-	public String what(String name) {
-		return "What?! the " + name;
+		return service.hello("Hi, " + name);
 	}
 	
 	@GetMapping("/sup")
-	public int baliw(@RequestParam (required = false, defaultValue = "0") int num) {
-		return num;
+	public String test(@RequestParam String name) {
+		return service.hi(name);
 	}
+	
+	@GetMapping("/tumesting/{num1}/{num2}")
+	public String dota(@PathVariable int num1,
+					   @PathVariable int num2) {
+		return service.minusNum(num1, num2);
+	}
+	
+	
 }
---
+++ src/main/java/typeshi/SpringBooty/service/TestInterface.java
@@ -0,0 +1,7 @@
+package typeshi.SpringBooty.service;
+
+public interface TestInterface {
+	String hi(String name);
+	String hello(String word);
+	String minusNum(int a, int b);
+}
